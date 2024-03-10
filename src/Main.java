e4b7cecd418d6bfbf7114a1d06afca7752c90641
Kerem Yardan <keremyrdn@hotmail.com>
2024-03-10 18:35:33 +0300

    comments have been revised

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //For matrix dimensions,the customer is being asked to enter two values.
+        //For matrix dimensions,the player is being asked to enter two values.
 
         Scanner input = new Scanner (System.in);
 
@@ -14,7 +14,7 @@ public class Main {
         System.out.println("Enter a value for B!");
         int col = input.nextInt();
 
-        // If the values are below 2, customer going to get a warning message about invalid data presentation. (refers to 4th chapter)
+        // If the values are below 2, player is going to get a warning message about invalid data presentation. (refers to 4th chapter)
         if(row<2 || col<2) {
             System.out.println("Array can not be smaller than 2");
         }else {
